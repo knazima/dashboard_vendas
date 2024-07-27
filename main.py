da095adc627d4e2c620a4c3cffaab098b9102f8e
knazima <keillanazima@gmail.com>
2024-07-26 21:36:40 -0300

    done: Projeto finalizado com sucesso.

--- main.py
+++ main.py
@@ -24,6 +24,10 @@ df['Mês'] = df['dt_Venda'].dt.strftime('%b').str.upper()
 
 #Listas de apoio________________________________________________________________________________________________________
 lista_meses = []
+
+df = df[df['Mês'].notna()]
+df = df[df['Mês'] != ' ']
+
 for mes in df['Mês'].unique():
     lista_meses.append({
         'label': mes,
@@ -145,11 +149,13 @@ linha_1 = html.Div([
 
 linha_2 = html.Div([
         dcc.Graph(id='visual02', style={'width': '65%'}),
-        dcc.Graph(id='visual03', style = {'width': '30%'})
+        dcc.Graph(id='visual03', style = {'width': '35%'})
 ], style={
     'display':'flex',
     'justify-content': 'space-around',
-    'height': '300px'
+    'height': '350px',
+    'margin-top': '20px',
+    'vertical-align': 'center'
 })
 
 
@@ -171,7 +177,6 @@ app.layout = html.Div([
         Input('dropdown_clientes', 'value')
 
 )
-
 def atualizar_txt(cliente_selecionado):
     if cliente_selecionado:
         return f'Top5 Produtos comprados por: {cliente_selecionado}'
@@ -237,6 +242,122 @@ def visual01(cliente, mes, categoria,toggle):
     )
     return fig1
 
+@app.callback(
+    [
+    Output('visual02', 'figure'),
+    Output('visual03', 'figure')
+    ],
+    [
+        Input('radio_categoria', 'value'),
+        Input('radio_meses', 'value'),
+        Input(ThemeSwitchAIO.ids.switch('theme'), 'value')
+    ]
+)
+def visual02(categoria, mes, toggle):
+
+    template = dark_tema if toggle else vapor_tema
+    
+    nome_categoria = filtro_categoria(categoria)
+    nome_mes = filtro_mes(mes)
+
+    filtros_categoria = nome_categoria
+    filtro_mes_categoria = nome_categoria & nome_mes
+
+    df2 = df.loc[filtros_categoria]
+    df3 = df.loc[filtro_mes_categoria]
+
+    #agrupar total de vendas por loja
+    df_lojas_todas2 = df2.groupby(['Mês', 'Loja'])['Total'].mean().reset_index()
+
+    df_lojas_todas3 = df3.groupby(['Mês', 'Loja'])['Total'].sum().reset_index()
+
+    max_size = df_lojas_todas2['Total'].max() 
+    min_size = df_lojas_todas2['Total'].min()
+
+
+    cores_map = {
+        'Rio de Janeiro' : '#4cf55a',
+        'Salvador'       : '#63dbd1',
+        'santos'         : '#f54cbf',
+        'São Paulo'      : '#f54c62',
+        'Três Rios'      : '#fff'
+    }
+    
+    ordem_meses_x = ['JAN','FEB','MAR','ABR','MAY','JUN','JUL','AUG','SEP','OCT','NOV','DEC']
+
+    fig2 = go.Figure()
+
+    for loja in df_lojas_todas2['Loja'].unique():
+        df_lojas = df_lojas_todas2[df_lojas_todas2['Loja'] == loja ]
+        cor = cores_map.get(loja,'Black')
+
+        fig2.add_trace(
+            go.Scatter(
+                x = df_lojas['Mês'],
+                y = df_lojas['Total'],
+                mode='markers',
+                marker=dict(
+                    color=cor,
+                    size = (df_lojas['Total'] - min_size)/(max_size-min_size)*50,
+                    opacity = 0.5,
+                    line=dict(color=cor)
+                ),
+                name=str(loja)
+            )
+        )
+
+    fig2.update_layout(
+        margin=dict(t=0),
+        template=template,
+        plot_bgcolor = 'rgba(0,0,0,0)',
+        paper_bgcolor = 'rgba(0,0,0,0)',
+        xaxis=dict(
+            categoryorder = 'array',
+            categoryarray = ordem_meses_x,
+            showgrid=False
+        ),
+        yaxis=dict(showgrid=False)
+    )
+
+    # visual03__________________________________________________________________________________________________________
+
+    fig3 = go.Figure(data=go.Scatterpolar(
+        r=df_lojas_todas3['Total'],
+        theta=df_lojas_todas3['Loja'],
+        line=dict(color= 'rgb(31,119,180)'),
+        marker=dict(color= 'rgb(31,119,180)', size=7),
+        fill='toself',
+        opacity=0.8
+        
+    ))
+
+    fig3.update_layout(
+        template=template,
+        polar=dict(
+            radialaxis=dict(
+                visible=False,
+                tickfont=dict(size=10),
+                tickangle=0,
+                tickcolor='rgba(68,68,6,0)',
+                ticklen=5,
+                tickwidth=1,
+                tickprefix='',
+                ticksuffix='',
+                range=[0, max(df_lojas_todas3['Total']) + 1000]
+            )
+        ),
+        font=dict(
+            family='Fira Code', 
+            size=12
+        ),
+        plot_bgcolor = 'rgba(0,0,0,0)',
+        paper_bgcolor = 'rgba(0,0,0,0)',
+        margin=dict(l=40, r=40, t=40, b=40)
+    )
+
+
+
+    return fig2, fig3
 
 
 #Subindo servidor_______________________________________________________________________________________________________
